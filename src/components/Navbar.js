267e84c556072a5b5b89982ee8e53879f136a3b6
PiyushSandhan <112093880+PiyushSandhan@users.noreply.github.com>
2023-07-06 19:09:12 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+//import PropTypes from 'prop-types'
+
+export class navbar extends Component {
+  // static propTypes = {
+
+  // }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg bg-primary">
+  <div className="container-fluid">
+    
+    <a className="navbar-brand" href="/">Daily-News</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+     
+      
+    </div>
+  </div>
+</nav>
+      </div>
+    )
+  }
+}
+
+export default navbar
